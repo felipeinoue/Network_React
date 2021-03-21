86d4b481420b1955b4af57de9f80df871cf2a1e6
Felipe Inoue <felipeinoue@hotmail.com>
2021-03-21 22:51:54 +0100

    add models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_post")
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, null=False)
+    likes = models.ManyToManyField('User')
+
+
+class Follow(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_follow")
+    following = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following_follow")
